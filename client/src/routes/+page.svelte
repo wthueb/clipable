ae12281a56f29816cdc54640709bc62ef7033772
William Huebner <wilhueb@gmail.com>
2023-11-11 04:33:45 -0500

    custom error page, better test data

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -6,16 +6,23 @@
 </script>
 
 <div>
-    dashboard
+    <button
+        class="m-3 transform rounded bg-cyan-900 px-3 py-2 text-white transition-transform active:scale-95"
+        on:click={() => $addClip.mutate({ title: `test ${new Date().toISOString()}` })}
+    >
+        add clip
+    </button>
+</div>
 
-    <button on:click={() => $addClip.mutate({ title: 'test' })}> add clip </button>
-
+<div>
     <div>
         {#if $clips.isSuccess}
+            <h1 class="text-2xl">clips</h1>
             {#each $clips.data as clip}
                 <span>
                     {clip.title}
                 </span>
+                <br />
             {/each}
         {/if}
     </div>
